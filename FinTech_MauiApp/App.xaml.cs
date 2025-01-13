92e1a7eae9c9c71a98d8e02afb7a32a710bfa9cf
spgh-ghub <i.new2learn@gmail.com>
2025-01-13 16:33:24 -0500

    Native Cross Platform Apps with .NET MAUI (Multi-platform Application User Interface)

--- FinTech_MauiApp/App.xaml.cs
+++ FinTech_MauiApp/App.xaml.cs
@@ -11,5 +11,22 @@
         {
             return new Window(new AppShell());
         }
+
+        // Optional events to implement for the application lifecycle. The Window created 
+        // above also has lifecycle events that can be used to track the Window lifecycle.
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+        }
     }
 }
