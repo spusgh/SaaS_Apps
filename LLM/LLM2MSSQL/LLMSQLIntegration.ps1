3e40ceedcc53a54dcf665cc5d3a0196c66d65a76
spusgh <shailapatel@outlook.com>
2025-07-28 15:55:12 -0400

    LLM Tiny Lama 2 MS SQL

---
+++ LLM/LLM2MSSQL/LLMSQLIntegration.ps1
@@ -0,0 +1,22 @@
+﻿/#
+
+-- to kill all ollama process - repeat for each process id
+PS C:\Users\Shail> Get-Process -Name ollama | Select-Object -Property Id
+
+   Id
+   --
+16272
+
+
+PS C:\Users\Shail> Stop-Process -Id 16272
+
+
+#/
+
+taskkill /F /IM ollama.exe
+Stop-Service -Name "ollama"
+Start-Process "ollama" -ArgumentList "serve" -WindowStyle Hidden
+ 
+ollama start
+ollama list
+
